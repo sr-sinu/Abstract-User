87cec4e9c1dbef22fbe9bbcc3ea473d7328d8048
sr-sinu <starunofficial@gmail.com>
2023-10-21 12:21:47 +0530

    changed db in settings and new media file added

--- ABUser/ABUser/settings.py
+++ ABUser/ABUser/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'ABUser.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'abc',
-        'USER': 'root',
-        'PASSWORD': 'groot',
-        'HOST': 'localhost',
-        'PORT': 3306,
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'abc',
+    #     'USER': 'root',
+    #     'PASSWORD': 'groot',
+    #     'HOST': 'localhost',
+    #     'PORT': 3306,
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
